9e197843ac6e59fccccc971b4d107227cef61d28
Sorachat <73382296+ter2002@users.noreply.github.com>
2020-11-20 17:44:10 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,6 +5,14 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 <title>ITF Lab</title>
+<?php
+  $conn = mysqli_init();
+  mysqli_real_connect($conn, 'sorachat.mysql.database.azure.com', 'ter2002@sorachat', 'Zazaza5789', 'ITFLab', 3306);
+  if (mysqli_connect_errno($conn)) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+  }
+  $res = mysqli_query($conn, 'SELECT * FROM guestbook');
+  ?>
 </head>
 
 <body>
@@ -20,13 +28,27 @@
   </thead>
   <tbody>
   <?php
-  $conn = mysqli_init();
-  mysqli_real_connect($conn, 'sorachat.mysql.database.azure.com', 'ter2002@sorachat', 'Zazaza5789', 'ITFLab', 3306);
-  if (mysqli_connect_errno($conn)) {
-    die('Failed to connect to MySQL: ' . mysqli_connect_error());
-  }
-  $res = mysqli_query($conn, 'SELECT * FROM guestbook');
+    while ($Result = mysqli_fetch_array($res)) {
+    ?>
+      <tr>
+        <td><?php echo $Result['Name']; ?></div>
+        </td>
+        <td><?php echo $Result['Comment']; ?></td>
+        <td><?php echo $Result['Link']; ?></td>
+        <td><a href="delete.php?ID=<?php echo $Result['ID'];?>">Delete</a></td>
+        <td><a href="edit.php?ID=<?php echo $Result['ID'];?>">edit</a></td>
+      </tr>
+    <?php
+    }
+    ?>
+  </tbody>
+  <?php
+  mysqli_close($conn);
   ?>
+</table>
+
+
+  <!--
   <table width="600" border="1">
     <tr>
       <th width="100">
@@ -45,28 +67,8 @@
         <div align='center'>Edit</div>
       </th>
     </tr>
-    <?php
-    while ($Result = mysqli_fetch_array($res)) {
-    ?>
-      <tr>
-        <td><?php echo $Result['Name']; ?></div>
-        </td>
-        <td><?php echo $Result['Comment']; ?></td>
-        <td><?php echo $Result['Link']; ?></td>
-        <td><a href="delete.php?ID=<?php echo $Result['ID'];?>">Delete</a></td>
-        <td><a href="edit.php?ID=<?php echo $Result['ID'];?>">edit</a></td>
-      </tr>
-    <?php
-    }
-    ?>
-  </table>
-  <?php
-  mysqli_close($conn);
-  ?>
-  </tbody>
-</table>
-
-
+    
+  </table>-->
   
   <button><a href="form.html">Add user</a></button>
 </body>
