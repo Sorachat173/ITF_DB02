4946f59f4957f21f68b073ad2d99d87ee8e8ec8c
Sorachat <73382296+ter2002@users.noreply.github.com>
2020-11-20 15:59:47 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -18,7 +18,8 @@
 	<title>Comment Form</title>
 </head>
 <body>
-  <form action = "update.php" method = "post" id="CommentForm" >
+  <form action = "update.php" method = "post" >
+  <input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none" required>
     Name:<br>
     <input type="text" name = "name" id="idName" placeholder="Enter Name" value="<?php echo $data['Name']; ?>"> <br>
     Comment:<br>
