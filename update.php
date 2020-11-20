bcd45ed64b1f3ac5794d394acbea27624e5ddaee
Sorachat <73382296+ter2002@users.noreply.github.com>
2020-11-20 15:53:53 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,11 +1,11 @@
 <?php
 	$conn = mysqli_connect('sorachat.mysql.database.azure.com', 'ter2002@sorachat', 'Zazaza5789', 'ITFLab');
 
-	$name = $_POST['Name'];
-	$comment = $_POST['Comment'];
-	$link = $_POST['Link'];
+	$Name = $_POST['Name'];
+	$Comment = $_POST['Comment'];
+	$Link = $_POST['Link'];
 
-	$sql = 'UPDATE guestbook SET name = "'.$name.'", comment = "'.$comment.'", link = "'.$link.'" WHERE ID = '.$id.'';
+	$sql = 'UPDATE guestbook SET Name = "'.$Name.'", Comment = "'.$Comment.'", Link = "'.$Link.'" WHERE ID = '.$id.'';
 ?>
 <!DOCTYPE html>
 <html lang="en">
