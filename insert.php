82d034efa9b150d4756c2b2ab1426ca48bdd0831
Sorachat <73382296+ter2002@users.noreply.github.com>
2020-11-26 21:35:48 +0700

    ะำพ

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, '103.91.205.130', 'sorachat', 'CO22Xa6yXlaBST7Y', 'sorachat', 80);
+mysqli_real_connect($conn, '103.91.205.130', 'sorachat', 'CO22Xa6yXlaBST7Y', 'sorachat', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
